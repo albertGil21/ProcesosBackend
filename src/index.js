f075093febed0c3787accf27a0feaa5c48ca1b6f
albertGil21 <alberto.gil@unmsm.edu.pe>
2024-10-28 21:40:00 -0500

    modificando puerto vercel

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import gymRoutes from './routes/gym.routes.js';
 const app = express();
 
 app.use(express.json());
-app.use('/api',gymRoutes);
+app.use('/api', gymRoutes);
 
-app.listen(3000, () => {
-    console.log('Server on port', 3000); // Escucha en el puerto 3000
+const PORT = process.env.PORT || 3000; // Usa el puerto asignado por Vercel o 3000 como fallback
+
+app.listen(PORT, () => {
+    console.log('Server on port', PORT); // Escucha en el puerto asignado
 });
